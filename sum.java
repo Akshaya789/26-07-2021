7ee937fb8168b60deff67c5a3e2d29b51e2bf3d2
akshaya <akshaya@gmail.com>
2021-07-26 05:56:18 -0400

    my commit

---
+++ sum.java
@@ -0,0 +1,13 @@
+#!/bin/bash #!/bin/bash
+#Prints #Prints
+“Hello World Hello World
+” and exists and exists
+echo
+“Hello World Hello World
+”
+echo
+“$USER, your current directory is $PWD $USER, your current directory is $PWD
+”
+echo `ls
+`
+exit #Clean way to exit a shell script
